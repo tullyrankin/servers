318a8a43736c311a45b47721ee8e92a401afaddd
Tully Rankin <tullyrankin@wireless-host-248-241.expo.socallinuxexpo.org>
2014-02-22 12:38:43 -0800

    Added nagios plugins to nagios module

--- nagios/puppet/modules/nagios/manifests/init.pp
+++ nagios/puppet/modules/nagios/manifests/init.pp
@@ -17,4 +17,13 @@ class nagios {
 		source => "puppet:///modules/nagios/htpasswd.users",
 		notify => Service['nagios3']
 	}
+
+	file { "/usr/lib/nagios/plugins":
+		recurse => true,
+		source => "puppet:///modules/nagios/plugins",
+		owner => nagios,
+		group => nagios,
+		mode => 0755,
+		require => Package['nagios3']
+	}
 }
